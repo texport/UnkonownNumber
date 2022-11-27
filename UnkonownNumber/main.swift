a53060ab4c3dc9ffdfe53a329a213c8b4557c778
Sergey Ivanov <ivanov.sergey.ekb@gmail.com>
2022-11-27 21:52:07 +0600

    м1

--- UnkonownNumber/main.swift
+++ UnkonownNumber/main.swift
@@ -7,5 +7,41 @@
 
 import Foundation
 
-print("Hello, World!")
 
+var answerHuman: String?
+var isTrue = false
+
+repeat {
+    print("Вы хотите сыграть в игру отгадай число, если хотите, то напишите 'Новая игра' или напишите 'Выход' если хотите выйти")
+    answerHuman = readLine()
+    if answerHuman!.contains("Новая игра") {
+        startGame()
+    } else if answerHuman!.contains("Выход") {
+        isTrue = true
+    } else {
+        print("Вы ввели что то не то, попробуйте снова")
+    }
+} while !isTrue
+
+func startGame() {
+    var isWin = false
+    let randomNumber = UInt8.random(in: 1...250)
+    
+    print("Компьютер случайным образом загадал число. Вам требуется отгадать его.")
+    print("Введите вариант от 1 до 250 и нажмите Enter.")
+    
+    repeat {
+        guard let userNumber = UInt8(readLine() ?? "") else {
+            print("Вы ввели некорректное число. Попробуйте снова")
+            continue
+        }
+        if userNumber < randomNumber {
+            print("Ваш вариант меньше заданного числа")
+        } else if userNumber > randomNumber {
+            print("Ваш вариант больше заданного числа")
+        } else {
+            print("Вы угадали")
+            isWin = true
+        }
+    } while !isWin
+}
